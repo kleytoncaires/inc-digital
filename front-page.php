87fb495686c470beed56aa6d4006aa68a9b8ca9d
Kleyton Caires <kleytoncaires@gmail.com>
2024-10-10 00:00:28 -0300

    feature: blank theme

--- front-page.php
+++ front-page.php
@@ -1,11 +1,5 @@
 <?php get_header(); ?>
 
-<?php get_template_part('partials/embed-video', null, [
-    'video_link' => $video_link,      // Link to the video
-    'modal' => true,                   // Set to true for modal, false for direct display
-    // 'thumbnail_image' => $thumbnail_image // Custom thumbnail image, if available
-]); ?>
-
-<?php get_template_part('partials/loop-ajax'); ?>
+<?php get_template_part('partials/'); ?>
 
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
 
-<?php get_template_part('partials/'); ?>
+<?php get_template_part('partials/article'); ?>
 
 <?php get_footer(); ?>
