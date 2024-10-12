76975562068b9a1cdc96d73fa1708da204dca478
Kleyton Caires <kleytoncaires@gmail.com>
2024-10-12 01:23:01 -0300

    feature: sample whatsapp

---
+++ samples/partials/whatsapp.php
@@ -0,0 +1,10 @@
+<?php
+$whatsapp = get_field('whatsapp', 'option');
+$linkWhatsapp = preg_replace('/\D/', '', $whatsapp);
+?>
+
+<?php if ($whatsapp) : ?>
+    <a href="https://wa.me/+55<?php echo $linkWhatsapp ?>" class="" target="_blank">
+        <?php echo $whatsapp; ?>
+    </a>
+<?php endif; ?>
