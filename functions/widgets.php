4aae3a88634019f99c6c795bcc2f9fea01efb3bc
Kleyton Caires <kleytoncaires@gmail.com>
2024-10-15 15:41:02 -0300

    feature: update in functions

--- functions/widgets.php
+++ functions/widgets.php
@@ -3,16 +3,16 @@
 // ----------- 
 // REGISTER WIDGETS
 // -----------
-function widget_support()
-{
-    register_sidebar(array(
-        'name'          => 'Theme Widgets',
-        'id'            => 'theme-widgets',
-        'description'   => '',
-        'before_widget' => '<aside>',
-        'after_widget'  => '</aside>',
-        'before_title'  => '<h3>',
-        'after_title'   => '</h3>',
-    ));
-}
-add_action('widgets_init', 'widget_support');
+// function widget_support()
+// {
+//     register_sidebar(array(
+//         'name'          => 'Theme Widgets',
+//         'id'            => 'theme-widgets',
+//         'description'   => '',
+//         'before_widget' => '<aside>',
+//         'after_widget'  => '</aside>',
+//         'before_title'  => '<h3>',
+//         'after_title'   => '</h3>',
+//     ));
+// }
+// add_action('widgets_init', 'widget_support');
