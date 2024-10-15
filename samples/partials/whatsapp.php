d6ab663bf571bcb1c9a129a98962c6c32d8887af
Kleyton Caires <kleytoncaires@gmail.com>
2024-10-14 22:12:56 -0300

    feature: country code in whatsapp

--- samples/partials/whatsapp.php
+++ samples/partials/whatsapp.php
@@ -4,7 +4,7 @@ $linkWhatsapp = preg_replace('/\D/', '', $whatsapp);
 ?>
 
 <?php if ($whatsapp) : ?>
-    <a href="https://wa.me/+55<?php echo $linkWhatsapp ?>" class="" target="_blank">
+    <a href="https://wa.me/55<?php echo $linkWhatsapp ?>" class="" target="_blank">
         <?php echo $whatsapp; ?>
     </a>
 <?php endif; ?>
